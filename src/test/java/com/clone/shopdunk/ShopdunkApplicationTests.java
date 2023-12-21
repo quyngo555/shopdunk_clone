d43d1957788fb932beff7f0c23f265aca88f8db8
quyngo <quyngo555@gmail.com>
2023-12-21 21:07:56 +0700

    init project

---
+++ src/test/java/com/clone/shopdunk/ShopdunkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.shopdunk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopdunkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
